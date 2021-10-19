ef08de7d1eb77c4a31ba65b6ffe5545ebd4c62bb
usman1607 <usman1607@yahoo.com>
2021-10-19 12:33:33 +0100

    commit

---
+++ src/test/java/com/websocket/chatdemo/ChatdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.chatdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
